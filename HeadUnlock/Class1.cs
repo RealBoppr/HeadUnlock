5276b0c1f30cbcaf2568e9acb13616b75104c306
Cloudie <introport777@gmail.com>
2021-12-12 14:46:32 -0500

    Add project files.

---
+++ HeadUnlock/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using VRC.DataModel;
+
+namespace HeadUnlock
+{
+    public class Class1
+    {
+        public class MyMod : MelonMod
+        {
+            private NeckRange orgin;
+            public override void OnApplicationStart()
+            {
+                MelonLogger.Msg("Hold Alt to unlock head");
+            }
+            public override void OnApplicationLateStart()
+            {
+                //MelonLogger.Msg("Why are you still playing this game?");
+            }
+
+            public override void OnUpdate()
+            {
+                if (Input.GetKeyDown(KeyCode.LeftAlt))
+                {
+                    //Enables funny
+
+                    this.orgin = ((LocomotionInputController)((Component)VRCPlayer.field_Internal_Static_VRCPlayer_0).GetComponent<GamelikeInputController>()).field_Protected_NeckMouseRotator_0.field_Public_NeckRange_0;
+                    ((LocomotionInputController)((Component)VRCPlayer.field_Internal_Static_VRCPlayer_0).GetComponent<GamelikeInputController>()).field_Protected_NeckMouseRotator_0.field_Public_NeckRange_0 = new NeckRange(float.MinValue, float.MaxValue, 0.0f);
+                    
+                    MelonLogger.Msg("Unlocked Head Rotation");
+                }
+
+                if (Input.GetKeyUp(KeyCode.LeftAlt))
+                {
+                    //No more funny :(
+
+                    ((LocomotionInputController)((Component)VRCPlayer.field_Internal_Static_VRCPlayer_0).GetComponent<GamelikeInputController>()).field_Protected_NeckMouseRotator_0.field_Public_NeckRange_0 = this.orgin;
+
+                    MelonLogger.Msg("Locked Head Rotation");
+                }
+            }
+
+        }
+    }
+}
---
+++ HeadUnlock/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using System;
+using HeadUnlock;
+using static HeadUnlock.Class1;
+
+[assembly: AssemblyTitle("HeadUnlock")]
+[assembly: AssemblyDescription("Infinite Camera")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cloudie")]
+[assembly: AssemblyProduct("HeadUnlock")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b7d6ad61-b58c-49f0-822c-b05da7dd83cf")]
+[assembly: MelonColor(ConsoleColor.DarkBlue)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(MyMod), "HeadUnlocker", "1", "Cloudie")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
