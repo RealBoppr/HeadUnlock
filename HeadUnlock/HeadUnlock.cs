36726eec2a7187a5ce2a87b8cd7dfa1472894fa7
Boppr <crazy_boppr@live.com>
2021-12-13 18:13:46 +1100

    UwU

---
+++ HeadUnlock/HeadUnlock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using MelonLoader;
+using UnityEngine;
+using VRC;
+using VRC.DataModel;
+
+[assembly: MelonColor(ConsoleColor.DarkBlue)]
+[assembly: MelonGame("VRChat", "VRChat")]
+[assembly: MelonInfo(typeof(HeadUnlock.Main), "HeadUnlocker", "1.1", "Cloudie")]
+
+namespace HeadUnlock
+{
+    public class Main : MelonMod
+    {
+        private NeckMouseRotator Neck => Player.prop_Player_0._vrcplayer.GetComponent<GamelikeInputController>().field_Protected_NeckMouseRotator_0;
+        private string KeybindFile = $"{MelonUtils.UserDataDirectory}\\HeadUnlock.txt";
+        private NeckRange Orgin;
+        private KeyCode Keybind;
+        public override void OnApplicationStart()
+        {
+            if (!File.Exists(KeybindFile))
+            {
+                File.WriteAllText(KeybindFile, "LeftAlt");
+            }
+            Keybind = Enum.TryParse(File.ReadAllText(KeybindFile), out Keybind) ? Keybind : KeyCode.LeftAlt;
+        }
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(Keybind) && Player.prop_Player_0 != null)
+            {
+                Orgin = Neck.field_Public_NeckRange_0;
+                Neck.field_Public_NeckRange_0 = new NeckRange(float.MinValue, float.MaxValue, 0.0f);
+            }
+            if (Input.GetKeyUp(Keybind) && Player.prop_Player_0 != null)
+            {
+                Neck.field_Public_NeckRange_0 = Orgin;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HeadUnlock
-Basically lets you rotate your head where ever lol
+A simple mod to unlock your head rotation
+
+You can change the keybind by editing the file at `VRChat\UserData\HeadUnlock.txt`
+[Click here for a list of keybinds](https://docs.unity3d.com/ScriptReference/KeyCode.html)
