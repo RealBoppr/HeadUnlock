88308d500097442a8d994763a99f3a0004f62404
Boppr <crazy_boppr@live.com>
2022-02-24 02:33:48 +1100

    Added Toggle + Config

--- HeadUnlock/HeadUnlock.cs
+++ HeadUnlock/HeadUnlock.cs
@@ -1,51 +1,82 @@
 ﻿using System;
-using System.IO;
 using MelonLoader;
 using UnityEngine;
+using ComfyUtils;
 using VRC;
 using VRC.DataModel;
 
 [assembly: MelonColor(ConsoleColor.DarkBlue)]
 [assembly: MelonGame("VRChat", "VRChat")]
-[assembly: MelonInfo(typeof(HeadUnlock.Main), "HeadUnlocker", "1.2", "Cloudie")]
+[assembly: MelonInfo(typeof(HeadUnlock.Main), "HeadUnlocker", "1.3", "Boppr (Original By Cloudie)")]
 
 namespace HeadUnlock
 {
     public class Main : MelonMod
     {
         private NeckMouseRotator Neck => Player.prop_Player_0._vrcplayer.GetComponent<GamelikeInputController>().field_Protected_NeckMouseRotator_0;
-        private string KeybindFile = $"{MelonUtils.UserDataDirectory}\\HeadUnlock.txt";
-        private NeckRange Orgin;
-        private KeyCode Keybind;
+        private Config Config => Helper.Config;
+        private ConfigHelper<Config> Helper;
+        private bool Unlocked = false;
+        private KeyCode PressKeybind;
+        private KeyCode HoldKeybind;
+        private NeckRange Origin;
         public override void OnApplicationStart()
         {
-            if (!File.Exists(KeybindFile))
+            Helper = new ConfigHelper<Config>($"{MelonUtils.UserDataDirectory}\\HeadUnlockConfig.json");
+            Helper.OnConfigUpdated += delegate
             {
-                File.WriteAllText(KeybindFile, "LeftAlt");
-            }
-            Keybind = Enum.TryParse(File.ReadAllText(KeybindFile), out Keybind) ? Keybind : KeyCode.LeftAlt;
-            FileSystemWatcher watcher = new FileSystemWatcher(Path.GetDirectoryName(KeybindFile), Path.GetFileName(KeybindFile))
-            {
-                NotifyFilter = NotifyFilters.LastWrite,
-                EnableRaisingEvents = true
+                KeyCode lastHoldKeybind = HoldKeybind;
+                HoldKeybind = Enum.TryParse(Config.HoldKeybind, out HoldKeybind) ? HoldKeybind : lastHoldKeybind;
+                KeyCode lastPressKeybind = PressKeybind;
+                PressKeybind = Enum.TryParse(Config.PressKeybind, out PressKeybind) ? PressKeybind : lastPressKeybind;
             };
-            watcher.Changed += delegate (object obj, FileSystemEventArgs args)
-            {
-                KeyCode lastKeybind = Keybind;
-                Keybind = Enum.TryParse(File.ReadAllText(KeybindFile), out Keybind) ? Keybind : lastKeybind;
-            };
+            HoldKeybind = Enum.TryParse(Config.HoldKeybind, out HoldKeybind) ? HoldKeybind : KeyCode.LeftAlt;
+            PressKeybind = Enum.TryParse(Config.PressKeybind, out PressKeybind) ? PressKeybind : KeyCode.H;
         }
         public override void OnUpdate()
         {
-            if (Input.GetKeyDown(Keybind) && Player.prop_Player_0 != null)
+            if (Player.prop_Player_0 == null)
             {
-                Orgin = Neck.field_Public_NeckRange_0;
-                Neck.field_Public_NeckRange_0 = new NeckRange(float.MinValue, float.MaxValue, 0.0f);
+                return;
             }
-            if (Input.GetKeyUp(Keybind) && Player.prop_Player_0 != null)
+            if (Config.Toggle)
             {
-                Neck.field_Public_NeckRange_0 = Orgin;
+                if (Input.GetKey(HoldKeybind) && Input.GetKeyDown(PressKeybind))
+                {
+                    ToggleUnlock(!Unlocked);
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(HoldKeybind))
+                {
+                    ToggleUnlock(true);
+                }
+                if (Input.GetKeyUp(HoldKeybind))
+                {
+                    ToggleUnlock(false);
+                }
             }
         }
+        private void ToggleUnlock(bool toggle)
+        {
+            if (toggle)
+            {
+                Origin = Neck.field_Public_NeckRange_0;
+                Neck.field_Public_NeckRange_0 = new NeckRange(float.MinValue, float.MaxValue, 0.0f);
+                Unlocked = true;
+            }
+            else
+            {
+                Neck.field_Public_NeckRange_0 = Origin;
+                Unlocked = false;
+            }
+        }
+    }
+    public class Config
+    {
+        public string HoldKeybind { get; set; } = "LeftAlt";
+        public string PressKeybind { get; set; } = "H";
+        public bool Toggle { get; set; } = true;
     }
 }
